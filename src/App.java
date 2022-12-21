04d16b83fabc33b81ada49babb8e977c772e4ce0
Yurag <arnieyurag@gmail.com>
2022-12-21 12:14:26 +0800

    Show the GUI

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class App extends JFrame{
     private JPanel pnlMain;
     private JRadioButton rbCustomer;
     private JRadioButton rbClerk;
@@ -30,6 +30,12 @@ public class App {
 
     public static void main(String[] args) {
         // add here how to make GUI visible
+        App app = new App();
+        app.setContentPane(app.pnlMain);
+        app.setSize(600, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+        app.setTitle("13th Month Pay");
     }
 
     static void giveReward(int n) {
